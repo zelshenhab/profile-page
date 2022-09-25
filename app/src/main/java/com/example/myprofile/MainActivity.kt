49d84f1e89c8153ee26e9c68ee6d011167569610
zelshenhab <z.elshenhab2012@gmail.com>
2022-09-25 21:59:28 +0300

    init

---
+++ app/src/main/java/com/example/myprofile/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myprofile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
